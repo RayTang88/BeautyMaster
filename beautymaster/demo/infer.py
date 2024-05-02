eac7e5af1badad54a50ea0aecef9fcaf793f07f5
Raytang88 <tc251369779@163.com>
2024-05-02 18:43:58 +0800

    add infer.py

--- beautymaster/demo/infer.py
+++ beautymaster/demo/infer.py
@@ -3,9 +3,9 @@ import torch
 def main():
     #1.Load caption and template to form prompt
 
-    #2.Trained LLM give advice
+    #2.Trained LLM give suggestions
 
-    #3.Virtual Try-on according the advice
+    #3.Virtual Try-on according the suggestions
 
     #4.Visualize the results of the suggestions to the user
     pass
