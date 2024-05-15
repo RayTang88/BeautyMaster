2005596e9e5cba3f9b9a4d9f865bf2a47860a994
RayTang88 <tc251369779@163.com>
2024-05-15 20:23:05 +0800

    change infer_internvl1-5_lmdeploy_cn.py

--- beautymaster/utils/infer_internvl1-5_lmdeploy_cn.py
+++ beautymaster/utils/infer_internvl1-5_lmdeploy_cn.py
@@ -3,6 +3,7 @@ from lmdeploy.vl import load_image
 import os
 import json
 from json_repair import repair_json
+import random
 
 
 def save_json(data, json_name):
@@ -14,7 +15,7 @@ def save_json(data, json_name):
         # ensure_ascii：保证了 “篮球” 能正确的写入，而不是字节形式
         # indent=4：为了美观，不然会保存成一行
 
-def label_clothes():
+def label_clothes(root_dir):
     # 要嵌入的JSON数据
     const_prompt="""{
         "version": "0.0.1",
@@ -37,7 +38,7 @@ def label_clothes():
             },
             {
             "from": "human",
-            "value": "请问这件衣服衣袖长度如何？长袖、短袖、无袖、或者其他？"
+            "value": "请问这件衣服衣袖长度如何？长袖、中袖、短袖、无袖、或者其他？"
             },
             {
             "from": "gpt",
@@ -59,6 +60,7 @@ def label_clothes():
             "from": "gpt",
             "value": "<answer5>"
             },
+            {
             "from": "human",
             "value": "请问这件衣服是什么颜色？"
             },
@@ -66,12 +68,21 @@ def label_clothes():
             "from": "gpt",
             "value": "<answer6>"
             },
+            {
             "from": "human",
             "value": "请问这件衣服的面料是什么？纯棉、化纤、尼龙、或者其他？"
             },
             {
             "from": "gpt",
             "value": "<answer7>"
+            },
+            {
+            "from": "human",
+            "value": "请问这件衣服是什么版型？宽松、修身、或者其他？"
+            },
+            {
+            "from": "gpt",
+            "value": "<answer8>"
             }
         ]    
     }"""
@@ -85,20 +96,117 @@ def label_clothes():
 
 
     pipe = pipeline('/root/model/InternVL-Chat-V1-5/',
-                    backend_config=TurbomindEngineConfig(session_len=12288))
+                    backend_config=TurbomindEngineConfig(session_len=8190))
 
+    images = os.listdir(root_dir + "/images/") 
     for image in images:
-        prompts = [('%s'%prompt, load_image(root_dir + "/cloth/" + image))]
+        prompts = [('%s'%prompt, load_image(root_dir + "/images/" + image))]
         response = pipe(prompts)
 
         # print(response[0].text[7:-3])
         try:
             # json_obj = json.loads(response[0].text[7:-3])
-            good_json_string = repair_json(response[0].text)
-            json_name =  root_dir + "/json/"  + image[:-4]+".json"
-            save_json(good_json_string, json_name)
+            good_json_obj = repair_json(response[0].text, return_objects=True)
+
+            json_path = root_dir + "/json/"
+
+            if not os.path.exists(json_path):
+                os.makedirs(json_path)
+            json_name =  json_path  + image[:-4]+".json"
+            save_json(good_json_obj, json_name)
         except json.decoder.JSONDecodeError as e:
             print("JSONDecodeError:", str(e))
+
+def label_trous(root_dir):
+    # 要嵌入的JSON数据
+    const_prompt="""{
+        "version": "0.0.1",
+        "conversations":[
+            {
+            "from": "human",
+            "value": "<image>\n请问这条裤子适合什么季节穿？春、夏、秋、冬？."
+            },
+            {
+            "from": "gpt",
+            "value": "<answer1>"
+            },
+            {
+            "from": "human",
+            "value": "请问这条裤子的风格怎么样？牛仔裤、休闲裤、正装、运动裤、或者其他？"
+            },
+            {
+            "from": "gpt",
+            "value": "<answer2>"
+            },
+            {
+            "from": "human",
+            "value": "请问这条裤子长度如何？长裤、短裤、超短裤、或者其他？"
+            },
+            {
+            "from": "gpt",
+            "value": "<answer3>"
+            },
+            {
+            "from": "human",
+            "value": "请问这条裤子是什么版型？宽松、修身、直通、喇叭、或者其他？"
+            },
+            {
+            "from": "gpt",
+            "value": "<answer4>"
+            },
+            "from": "human",
+            "value": "请问这条裤子是什么颜色？"
+            },
+            {
+            "from": "gpt",
+            "value": "<answer5>"
+            },
+            "from": "human",
+            "value": "请问这条裤子的面料是什么？纯棉、化纤、尼龙、或者其他？"
+            },
+            {
+            "from": "gpt",
+            "value": "<answer6>"
+            }
+        ]    
+    }"""
+
+    # 将JSON数据转换为字符串，并转义双引号和花括号
+    # json_string = json.dumps(const_prompt).replace('"', '\\"').replace('{', '\\{').replace('}', '\\}')
+    json_string = const_prompt.replace('"', '\\"').replace('{', '\\{').replace('}', '\\}').replace('\n', '\\n')
+    
+    # 将转义后的JSON字符串嵌入提示中
+    prompt = f"按照以下格式为我创建一个数据集:\n {json_string} \n要求：根据提供的图片,按json_string的格式生成问题和答案对。答案要必须精简，不要啰嗦废话，后面有选项的在选项中选即可。保证答案正确，不能瞎编，必须严格来源于图像内容中，如果图像没有，请回答不知道即可。最后只需要输出生成的json_string部分即可，不需要其他多余的部分。"
+
+
+    pipe = pipeline('/root/model/InternVL-Chat-V1-5/',
+                    backend_config=TurbomindEngineConfig(session_len=8190))
+
+    images = os.listdir(root_dir + "/images/")   
+    random.shuffle(images)
+
+    iamges = images[:10000]    
+
+
+    for image in images:
+        if image[-5:] == "1.jpg":
+            continue
+        prompts = [('%s'%prompt, load_image(root_dir + "/images/" + image))]
+        response = pipe(prompts)
+
+        # print(response[0].text[7:-3])
+        try:
+            # json_obj = json.loads(response[0].text[7:-3])
+            good_json_obj = repair_json(response[0].text, return_objects=True)
+
+            json_path = root_dir + "/json/"
+
+            if not os.path.exists(json_path):
+                os.makedirs(json_path)
+            json_name =  json_path  + image[:-4]+".json"
+            save_json(good_json_obj, json_name)
+        except json.decoder.JSONDecodeError as e:
+            print("JSONDecodeError:", str(e))            
 
 
 def label_model(image_path, json_path):
@@ -191,7 +299,9 @@ def label_model(image_path, json_path):
     pipe = pipeline('/root/model/InternVL-Chat-V1-5/',
                     backend_config=TurbomindEngineConfig(session_len=8190))
 
-    images = os.listdir(image_path)                
+    images = os.listdir(image_path)
+
+                    
 
     for image in images:
         prompts = [('%s'%prompt, load_image(image_path + image))]
@@ -200,13 +310,18 @@ def label_model(image_path, json_path):
         # print(response[0].text[7:-3])
         try:
             # json_obj = json.loads(response[0].text[7:-3])
-            good_json_string = repair_json(response[0].text)
-            json_name =  root_dir + "/json/"  + image[:-4]+".json"
-            save_json(good_json_string, json_name)
+            good_json_obj = repair_json(response[0].text, return_objects=True)
+            
+            json_path = root_dir + "/json/"
+
+            if not os.path.exists(json_path):
+                os.makedirs(json_path)
+            json_name =  json_path  + image[:-4]+".json"
+            save_json(good_json_obj, json_name)
         except json.decoder.JSONDecodeError as e:
             print("JSONDecodeError:", str(e))
    
-def label_mode_sup(image_path, json_path):
+def label_model_sup(image_path, json_path):
     # 要嵌入的JSON数据
     const_prompt="""{
         "conversations":[
@@ -250,7 +365,7 @@ def label_mode_sup(image_path, json_path):
     json_string = const_prompt.replace('"', '\\"').replace('{', '\\{').replace('}', '\\}').replace('\n', '\\n')
     
     # 将转义后的JSON字符串嵌入提示中
-    prompt = f"按照以下格式创建数据集:\n {json_string} \n根据提供的图片按json_string的格式生成问题和答案对。答案必须精简，后面有选项的在选项中选即可。答案必须严格来自图像内容，如果根据图像内容无法确定答案，请回答不知道。最后只输出生成的json_string部分即可。"
+    prompt = f"按照以下格式创建数据集:\n {json_string} \n根据提供的图片按json_string的格式生成问题和答案对。答案必须精简，后面有选项的在选项中选即可。答案必须严格来自图像内容，如果根据图像内容无法确定答案，请回答不知道。最后只输出生成的json_string部分即可。注意<answer>部分是hunman问题相对应的回答，请在（是、否、不知道）三个答案中选择，不要原样输出。"
 
 
     pipe = pipeline('/root/model/InternVL-Chat-V1-5/',
@@ -265,9 +380,12 @@ def label_mode_sup(image_path, json_path):
         # print(response[0].text[7:-3])
         try:
             # json_obj = json.loads(response[0].text[7:-3])
-            good_json_string = repair_json(response[0].text)
-            json_name =  root_dir + "/json_sup/"  + image[:-4]+".json"
-            save_json(good_json_string, json_name)
+            good_json_obj = repair_json(response[0].text, return_objects=True)
+
+            if not os.path.exists(json_path):
+                os.makedirs(json_path)
+            json_name =  json_path  + image[:-4]+".json"
+            save_json(good_json_obj, json_name)
         except json.decoder.JSONDecodeError as e:
             print("JSONDecodeError:", str(e))            
 
@@ -277,6 +395,16 @@ if __name__ == "__main__":
     # images = os.listdir(root_dir + "/cloth/")
     image_path = "/root/data/fullbody_cleaned_yolo_vl1_5/"
     json_path = "/root/data/fullbody_cleaned_yolo_vl1_5_json/"
-    label_model(image_path, json_path)
+    json_path_sup = "/root/data/fullbody_cleaned_yolo_vl1_5_json_sup/"
+
+    # label_model_sup(image_path, json_path_sup)
+
+    root_dir = "/root/data/DressCode/lower_body/"
+
+    # label_trous(root_dir)
+
+    root_dir = "/root/data/DressCode/upper_body/"
+
+    label_clothes(root_dir)
 
     
