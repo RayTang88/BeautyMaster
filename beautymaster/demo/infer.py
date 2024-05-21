ba27b71036391f62953398104220eac6cb2f46ed
Raytang88 <tc251369779@163.com>
2024-05-21 16:32:20 +0800

    update infer.py ...

--- beautymaster/demo/infer.py
+++ beautymaster/demo/infer.py
@@ -2,23 +2,26 @@ import argparse
 import torch
 import sys
 
-print(sys.path.append("./"))
+sys.path.append("/data0/tc_workspace/internlm/code/project/BeautyMaster/beautymaster")
 
 from src.infer_vlm import infer_vlm_func
+from src.infer_rag import infer_rag_func
 
 def run(weights='',  # model.pt path(s)
 		source='',  # file/dir/URL/glob, 0 for webcam
-		imgsz=640,  # inference size (pixels)
-		conf_thres=0.25,  # confidence threshold
 		save_path='',  # save results to project/name
+        get_num_list = [],
+        imgsz=640,  # inference size (pixels)
+		conf_thres=0.25,  # confidence threshold
         ):
     
     weather = "35~40摄氏度"
     season = "夏季"
     determine = "逛街"
     #1.Load caption and template to form prompt
-    model_candidate_clothes_list = rag_func()
+    
     #2.Trained VLM give suggestions
+    model_candidate_clothes_list = infer_rag_func(source, get_num_list) #for test, now get list randomly.
     response_string = infer_vlm_func(weights, model_candidate_clothes_list, season, weather, determine)
 
     #3.Virtual Try-on according the suggestions
@@ -28,13 +31,15 @@ def run(weights='',  # model.pt path(s)
 
 def parse_opt():
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--weights', nargs='+', type=str, default='', help='model path(s)')
-	parser.add_argument('--source', type=str, default='/data0/tc_workspace/data/vton/pic/pic/', help='')
-	parser.add_argument('--save_path', default='/data0/tc_workspace/internlm/code/project/py_child/data/save_data/', help='save results to project/name')
-	parser.add_argument('--imgsz', '--img', '--img-size', nargs='+',
-						type=int, default=[640, 480], help='inference size h,w')
-	parser.add_argument('--conf-thres', type=float,
-						default=0.3, help='confidence threshold')
+ 
+	parser.add_argument('--weights', nargs='+', type=str, default='/data0/tc_workspace/internlm/model/', help='model path(s)')
+	parser.add_argument('--source', type=str, default='/data0/tc_workspace/data/vton/test_data/', help='')
+ 
+	parser.add_argument('--save-path', type=str, default='/data0/tc_workspace/internlm/code/project/py_child/data/save_data/', help='save results to project/name')
+
+	parser.add_argument('--get-num-list', nargs='+', type=int, default=[1, 5, 5, 5], help='model and clothes number')
+	parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640, 480], help='inference size h,w')
+	parser.add_argument('--conf-thres', type=float, default=0.3, help='confidence threshold')
 
 	opt = parser.parse_args()
 	opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
@@ -42,7 +47,6 @@ def parse_opt():
 
 def main(opt):
 	run(**vars(opt))
-
 
 if __name__ == "__main__":
 	opt = parse_opt()
--- beautymaster/src/infer_vlm.py
+++ beautymaster/src/infer_vlm.py
@@ -12,14 +12,16 @@ def infer_vlm_func(weight, model_candidate_clothes_list, season, weather, determ
 
     backend_config = TurbomindEngineConfig(session_len=163840,  # 图片分辨率较高时请调高session_len
                                         cache_max_entry_count=0.2, 
-                                        tp=2)  # 两个显卡
+                                        tp=1,
+                                        # quant_policy=4,
+                                        )  # 两个显卡
 
-    pipe = pipeline(weight+"/Qwen-VL-Chat-Int4", backend_config=backend_config) 
+    pipe = pipeline(weight+"/Mini-InternVL-Chat-2B-V1-5/", backend_config=backend_config, ) 
 
     images = [load_image(model_candidate_clothes) for model_candidate_clothes in model_candidate_clothes_list]
     
     vlm_prompt = prompt.vlm_prompt_template.format("1", "2~6", "7~11", "12~16", season, weather, determine)
-    response = pipe(vlm_prompt, images)
+    response = pipe((vlm_prompt, images))
 
     # print(response.text)
     
