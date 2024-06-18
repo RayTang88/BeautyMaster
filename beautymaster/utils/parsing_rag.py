0bf7a20fe099cd3aff041db1a439be9a37d3c4ce
RayTang88 <tc251369779@163.com>
2024-06-18 15:40:05 +0800

    update session_len in infer_vlm.py

--- beautymaster/utils/parsing_rag.py
+++ beautymaster/utils/parsing_rag.py
@@ -18,13 +18,13 @@ def parsing_rag_func(rag_4o_like_recommended):
             content = recommend.page_content.split("\n")[1].replace("content: ", "")
             category = content.replace("content: ", "").split("、")[0]
             if category == "上衣" and category_en == "upper_body":
-                upper+=("idx: %s, content: %s"%(idx, content))
+                upper+=("idx: %s, content: %s; "%(idx, content))
             if category == "裤子" and category_en == "lower_body":
-                lower+=("idx: %s, content: %s"%(idx, content))
+                lower+=("idx: %s, content: %s; "%(idx, content))
             if category == "半身裙" and category_en == "lower_body":
-                skirt+=("idx: %s, content: %s"%(idx, content))
+                skirt+=("idx: %s, content: %s; "%(idx, content))
             if category == "连衣裙" and category_en == "dresses":
-                dresses+=("idx: %s, content: %s"%(idx, content))
+                dresses+=("idx: %s, content: %s; "%(idx, content))
 
     return upper, lower, skirt, dresses
     
