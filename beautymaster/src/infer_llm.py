0017d6637b8e5a0e0c3593c8d76f0396e6e980f9
RayTang88 <tc251369779@163.com>
2024-06-18 22:54:33 +0800

    update cache_max_entry_count

--- beautymaster/src/infer_llm.py
+++ beautymaster/src/infer_llm.py
@@ -7,12 +7,12 @@ from beautymaster.utils.parsing_rag import parsing_rag_func
 class LLM():
     def __init__(self, weights_path, weight_name, awq):
         # decrease the ratio of the k/v cache occupation to 20%
-        backend_config_awq = TurbomindEngineConfig(cache_max_entry_count=0.05,
+        backend_config_awq = TurbomindEngineConfig(cache_max_entry_count=0.07,
                                                model_format='awq',
-                                               session_len=10240)
+                                               session_len=16384)
         
-        backend_config = TurbomindEngineConfig(cache_max_entry_count=0.05,
-                                        session_len=10240)
+        backend_config = TurbomindEngineConfig(cache_max_entry_count=0.07,
+                                        session_len=16384)
         
         self.pipe = pipeline(weights_path + weight_name, backend_config=backend_config_awq if awq else backend_config, log_level='INFO') 
         
